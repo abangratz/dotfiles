0035d701a4e619858b2e7476b0d35164276e8f6e
Anton Bangratz <anton.bangratz@gmail.com>
2012-06-01 09:55:30 +0200

    Move dotfiles, add setup, add tmux(inator) conf

---
+++ setup.rb
@@ -0,0 +1,15 @@
+home = File.expand_path("~")
+target_dir = File.expand_path(File.dirname(__FILE__))
+
+dotfiles = Dir["*"] - %w[.git]
+
+p dotfiles
+exit
+
+dotfiles.each do |dotfile|
+  dotfilename = ".#{dotfile}"
+  original_file = File.join(home, dotfilename)
+  File.unlink original_file if File.symlink? original_file
+  File.rename original_file, "#{original_file}.bak" if File.exist? original_file
+  File.symlink File.join(target_dir, dotfile), original_file
+end
